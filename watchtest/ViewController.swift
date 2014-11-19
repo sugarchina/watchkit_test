f65ca7032935afeb30e6a4ef782dd6046723dd28
Roger <gooapi@gmail.com>
2014-11-19 14:16:32 -0500

    init the

---
+++ watchtest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  watchtest
+//
+//  Created by Roger on 2014-11-19.
+//  Copyright (c) 2014 Roger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
